3cf379e102e54915cb45a11827dcf5e394fbe386
dhavalpanchal01 <109593182+dhavalpanchal01@users.noreply.github.com>
2022-07-22 13:49:59 +0530

    Add files via upload

---
+++ AreaOfRectSqu.java
@@ -0,0 +1,24 @@
+// This is a program to find a area of rectangle and square
+
+
+class AreaOfRectSqu
+{
+public static void main(String args[])
+
+{
+
+float areaOfRect , areaOfSquare , sides , len , wid;
+
+sides=10;
+len=20;
+wid=5;
+
+areaOfRect=len*wid;
+areaOfSquare=sides*sides;
+
+{
+System.out.println("the area of rectangle is"+areaOfRect);
+System.out.println("the area of square is"+areaOfSquare);
+}
+}
+}
