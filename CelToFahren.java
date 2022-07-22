d9a923a6819d79c0a3bbc43de4c0d5a8a54d187a
dhavalpanchal01 <109593182+dhavalpanchal01@users.noreply.github.com>
2022-07-22 13:50:19 +0530

    Add files via upload

---
+++ CelToFahren.java
@@ -0,0 +1,21 @@
+// This is a program to convert cslsius into fahrenhiet
+
+
+class CelToFahren
+{
+public static void main(String args[])
+
+{
+
+double celsius , fahrenhit , celToFahren;
+
+celsius=47;
+celToFahren=celsius*1.8+32;
+
+{
+
+System.out.println("celsius to fahrenheit is"+celToFahren);
+
+}
+}
+}
