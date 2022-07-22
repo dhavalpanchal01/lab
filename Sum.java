e4db839056995b083b11fc3eed0392b8515c9a4b
dhavalpanchal01 <109593182+dhavalpanchal01@users.noreply.github.com>
2022-07-22 13:46:31 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,23 @@
+class Sum
+{
+public static void main(String args[])
+{
+
+int a=10;
+int b=50;
+int sum=a+b;
+float avg=sum/2;
+float diff=a-b;
+{
+
+System.out.println("the sum is"+sum);
+System.out.println("the sum is"+avg);
+System.out.println("the sum is"+diff);
+
+
+
+}
+
+}
+
+}
