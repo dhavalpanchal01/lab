0bda492c6a37e138e439442362b2da8835416bab
dhavalpanchal01 <109593182+dhavalpanchal01@users.noreply.github.com>
2022-08-04 17:00:52 +0530

    Add files via upload

---
+++ EqualityArray.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class EqualityArray
+{
+
+public static void main(String args[])
+{
+int [] arr1 = new int [] {20,40.60.80.100};
+int [] arr2 = new int [] {20,40,60,80,100};
+System.out.println(Arrays.equals(arr1,arr2));
+}
+}
